59bd07c5803e5b3164e40472b1590c457287e47e
DavidCordero <davidncnc22@gmail.com>
2025-09-21 07:38:07 -0600

    ejercios_2_3_semana3

---
+++ Semana3/ejercio_2_secuancial.py
@@ -0,0 +1,16 @@
+# Declaracion de variables
+cantidad_noches = 0 
+precio_x_noche= 100
+descuento= 5
+total_a_cobrar=0
+
+
+# Entrada de datos
+cantidad_noches = int(input("ingrese la cantidad de noches que se va a quedar:"))
+                    
+# Operaciones
+total_a_cobrar = cantidad_noches * precio_x_noche * 1- (1- descuento/100)
+
+# Salida de datos
+
+print("total a cobrar es", total_a_cobrar)
