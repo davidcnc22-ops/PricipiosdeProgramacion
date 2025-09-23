200b5466d7810702da8f2833126db94eca592205
DavidCordero <davidncnc22@gmail.com>
2025-09-23 17:49:59 -0600

    ejercio_9_ secuenciales

---
+++ Semana3/ejercios_9_secuenciales.py
@@ -0,0 +1,37 @@
+# Declaracion de variables
+
+descuento_pan= 0.15
+descuento_tomate= 0.35
+descuento_queso= 0.25
+descuento_lechuga= 0.10 
+gasto_total=  2700 
+descuento_jamom=0
+
+
+# Entrada de datos
+
+costo_pan = 0.15* gasto_total
+costo_tomate =0.45* gasto_total
+costo_queso = 0.35* gasto_total
+costo_lechuga = 0.25* gasto_total
+costo_jamon = 0.10* gasto_total
+
+
+
+# Operaciones
+
+porcentaje_jamon_es= 100 - (descuento_pan + descuento_tomate + descuento_queso + descuento_lechuga)
+
+
+# Salida de datos
+
+print("Costo del Pan:", costo_pan, "colones")
+print("Costo del Tomate:", costo_tomate, "colones")
+print("Costo del Queso:", costo_queso, "colones")
+print("Costo de la Lechuga:", costo_lechuga, "colones")
+print("Costo del Jamón:", costo_jamon, "colones")
+
+
+
+
+
