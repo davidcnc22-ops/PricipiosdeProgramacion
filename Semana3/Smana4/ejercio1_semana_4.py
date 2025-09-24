0733354e328cc8d8416dc852e427d14b025a015c
DavidCordero <davidncnc22@gmail.com>
2025-09-23 20:12:53 -0600

    ejercicios_semana4

---
+++ Semana3/Smana4/ejercio1_semana_4.py
@@ -0,0 +1,12 @@
+# Declaracion de variables
+
+n=0
+# Entrada de datos
+n=int(input("ingrese el numero_ "))
+
+if n % 2 == 0:
+    print("el numero es par")
+else: print ("el numero es impar")
+
+
+
