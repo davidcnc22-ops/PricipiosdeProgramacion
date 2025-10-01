a871ffcacf49b954a7223d3e1bde52231e8231be
DavidCordero <davidncnc22@gmail.com>
2025-09-30 18:13:14 -0600

    actailzacion

--- semana2/clase2/clase2.py
+++ semana2/clase2/clase2.py
@@ -28,9 +28,9 @@ Se utiliza la funcion print()
 """
 
 # *******************************Entrada de datos***********************************************
-numero1= int(input("ingrese el primer numero"))
-numero2= int(input ("ingrese el segundo numero"))
-numero3= int(input ("ingrese el tercer numero"))
+numero1= int(input("ingrese el primer numero:"))
+numero2= int(input ("ingrese el segundo numero:"))
+numero3= int(input ("ingrese el tercer numero:"))
 
 #*****************************************Procedimiento de Datos************************************
 resultadoSuma= numero1 + numero2+ numero3 
